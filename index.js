20392381f4e7af43b9fac6c6eda8f016ad7adbb2
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2023-06-01 13:31:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const APP_PORT = 8080 || process.env.port;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const routes = require("./routes");
+
+routes(app);
+
+//dbInitialSetup()
+
+app.listen((APP_PORT) => {
+  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
+  console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
+});
